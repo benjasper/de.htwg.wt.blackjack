670b7343dbfe2c0e8f4da2ce75b8de9f307926d7
jasieben <jana.sieben@aol.de>
2020-12-17 01:02:52 +0100

    Added Vue files

---
+++ app/views/menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="playing-table">
+    <div id="heading">
+      <h1 style="text-align: center">
+        Game Menu
+      </h1>
+    </div>
+    <div style="text-align: center">
+      @if(name != "") {
+      <h2>Hello, @name</h2>
+      }
+      <a class="btn btn-dark btn-lg" role="button" href="@controllers.routes.HomeController.rules()">Rules</a>
+      <br>
+      <br>
+      <a class="btn btn-dark btn-lg" role="button" href="@controllers.routes.HomeController.playingfield()">Start Game</a>
+    </div>
+  </div>
+</template>
+
+<script>
+loggedIn = isLoggedIn();
+if ("@playerId" === "" && loggedIn === false) {
+  window.location.href = "/login";
+}
+if (loggedIn === false) {
+  logIn("@playerId", "@name")
+}
+</script>
+
+<style>
+/*CSS goes here*/
+</style>
