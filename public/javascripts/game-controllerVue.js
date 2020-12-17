49d89af7c6693d81e4360172d0290e7fa9872b71
Benjamin Jasper <jasper.benjamin@outlook.de>
2020-12-17 11:33:31 +0100

    vue stuff

--- public/javascripts/game-controllerVue.js
+++ public/javascripts/game-controllerVue.js
@@ -1,118 +1,134 @@
-console.log("Yes I'm here!")
+p1cardsValue = 0
+p2cardsValue = 0
+p3cardsValue = 0
 
-socket = new WebSocket('ws://localhost:9000/websocket');// Create WebSocket connection.
-
-function constructor() {
+class GameController {
 
     cardController = new CardController();
+    socket = new WebSocket('ws://localhost:9000/websocket');// Create WebSocket connection.
 
-    const self = this;
 
-    // Connection opened
-    this.socket.addEventListener('open', function (event) {
-        console.log("Connected.");
-        console.log(event);
-    });
+    constructor() {
 
-    this.socket.addEventListener('close', function (event) {
-        console.log("Closed");
-        console.log(event);
-    });
+        const self = this;
 
-    // Listen for messages
-    this.socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
-        const response = JSON.parse(event.data);
-        switch (response.action) {
-            case "NEWGAME":
-                self.cardController.clearDealerCards();
-                self.cardController.clearPlayerCards(0);
-                self.cardController.clearPlayerCards(1);
-                self.cardController.clearPlayerCards(2);
+        // Connection opened
+        this.socket.addEventListener('open', function (event) {
+            console.log("Connected.");
+            console.log(event);
+        });
 
-                $('#hitGame').attr("disabled", false);
-                $('#standGame').attr("disabled", false);
+        this.socket.addEventListener('close', function (event) {
+            console.log("Closed");
+            console.log(event);
+        });
 
-                if ('success' in response.game && response.game.success === false) {
-                    self.error(response.game.msg);
-                    return;
-                }
+        // Listen for messages
+        this.socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+            const response = JSON.parse(event.data);
+            switch (response.action) {
+                case "NEWGAME":
+                    self.cardController.clearDealerCards();
+                    self.cardController.clearPlayerCards(0);
+                    self.cardController.clearPlayerCards(1);
+                    self.cardController.clearPlayerCards(2);
 
-                $('#newGame').attr("disabled", true);
-
-                self.cardController.addFlippedCardToDealer()
-
-                response.game.playerCards.forEach((card) => {
-                    self.cardController.addCardToPlayer(card.card, 1)
-                });
-
-                self.cardController.addFlippedCardToDealer()
-
-                break;
-            case "GAMEHIT":
-                if ('success' in response.game && response.game.success === false) {
-                    self.error(response.game.msg);
-                    return;
-                }
-
-                self.cardController.addCardToPlayer(response.game.hitCard, 1)
-                console.log(response.game);
-
-                if (response.game.gameStates[response.game.gameStates.length - 1].gameState === "WAITING_FOR_INPUT") {
                     $('#hitGame').attr("disabled", false);
                     $('#standGame').attr("disabled", false);
 
-                    return;
-                }
+                    if ('success' in response.game && response.game.success === false) {
+                        self.error(response.game.msg);
+                        return;
+                    }
 
-                self.cardController.revealDealerCards(response.game.dealerCards);
-                $("#newGame").attr("disabled", false);
-                break;
-            case "GAMESTAND":
-                if ('success' in response.game && response.game.success === false) {
-                    self.error(response.game.msg);
-                    return;
-                }
-                self.cardController.revealDealerCards(response.game.dealerCards);
-                $("#newGame").attr("disabled", false);
-                break;
+                    $('#newGame').attr("disabled", true);
+
+                    self.cardController.addFlippedCardToDealer()
+
+                    response.game.playerCards.forEach((card) => {
+                        self.cardController.addCardToPlayer(card.card, 1)
+                    });
+
+                    self.cardController.addFlippedCardToDealer()
+
+                    break;
+                case "GAMEHIT":
+                    if ('success' in response.game && response.game.success === false) {
+                        self.error(response.game.msg);
+                        return;
+                    }
+
+                    self.cardController.addCardToPlayer(response.game.hitCard, 1)
+                    console.log(response.game);
+
+                    if (response.game.gameStates[response.game.gameStates.length - 1].gameState === "WAITING_FOR_INPUT") {
+                        $('#hitGame').attr("disabled", false);
+                        $('#standGame').attr("disabled", false);
+
+                        return;
+                    }
+
+                    self.cardController.revealDealerCards(response.game.dealerCards);
+                    $("#newGame").attr("disabled", false);
+                    break;
+                case "GAMESTAND":
+                    if ('success' in response.game && response.game.success === false) {
+                        self.error(response.game.msg);
+                        return;
+                    }
+                    self.cardController.revealDealerCards(response.game.dealerCards);
+                    $("#newGame").attr("disabled", false);
+                    break;
+            }
+        });
+    }
+
+    error(error) {
+        alert(error);
+    }
+
+    newGame(event) {
+        const request = {
+            "action": "newGame",
+            "playerId": getLoggedInPlayer().playerId
+        };
+
+        this.socket.send(JSON.stringify(request));
+    }
+
+    hitGame(event) {
+        $('#hitGame').attr("disabled", true);
+        $('#standGame').attr("disabled", true);
+
+        const request = {
+            "action": "gameHit",
+            "playerId": getLoggedInPlayer().playerId
+        };
+
+        this.socket.send(JSON.stringify(request));
+    }
+
+    gameStand(event) {
+        $('#hitGame').attr("disabled", true);
+        $('#standGame').attr("disabled", true);
+
+        const request = {
+            "action": "gameStand",
+            "playerId": getLoggedInPlayer().playerId
+        };
+
+        this.socket.send(JSON.stringify(request));
+    }
+}
+
+$(document).ready(function () {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            p1cardsValue: p1cardsValue,
+            p2cardsValue: p2cardsValue,
+            p3cardsValue: p3cardsValue
         }
-    });
-}
-
-function error(error) {
-    alert(error);
-}
-
-function newGame(event) {
-    const request = {
-        "action": "newGame",
-        "playerId": getLoggedInPlayer().playerId
-    };
-
-    this.socket.send(JSON.stringify(request));
-}
-
-function hitGame(event) {
-    $('#hitGame').attr("disabled", true);
-    $('#standGame').attr("disabled", true);
-
-    const request = {
-        "action": "gameHit",
-        "playerId": getLoggedInPlayer().playerId
-    };
-
-    this.socket.send(JSON.stringify(request));
-}
-
-function gameStand(event) {
-    $('#hitGame').attr("disabled", true);
-    $('#standGame').attr("disabled", true);
-
-    const request = {
-        "action": "gameStand",
-        "playerId": getLoggedInPlayer().playerId
-    };
-
-    this.socket.send(JSON.stringify(request));
-}
+    })
+})
