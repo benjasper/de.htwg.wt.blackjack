936b22e75ec495981bd7188c450dfe3f647b99eb
Benjamin Jasper <jasper.benni@gmail.com>
2021-01-13 14:26:20 +0100

    added css and stuff

--- blackjack-frontend/src/main.ts
+++ blackjack-frontend/src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import './assets/styles/bootstrap.min.css'
+import './assets/styles/main.less'
 
 Vue.config.productionTip = false
 
--- blackjack-frontend/src/views/Home.vue
+++ blackjack-frontend/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="playing-table">
+    <div id="heading">
+      <h1 style="text-align: center">
+        Game Menu
+      </h1>
+    </div>
+    <div style="text-align: center">
+      <h2>Name</h2>
+      <a class="btn btn-dark btn-lg" role="button">Rules</a>
+      <br>
+      <br>
+      <a class="btn btn-dark btn-lg" role="button">Start Game</a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld
-  }
 })
 export default class Home extends Vue {}
 </script>
