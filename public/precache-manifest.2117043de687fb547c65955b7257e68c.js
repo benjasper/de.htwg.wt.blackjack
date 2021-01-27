7910f0f5b8795a1d204598d5ac43d093f0e4ee8f
Benjamin Jasper <jasper.benni@gmail.com>
2021-01-27 13:19:51 +0100

    added public

---
+++ public/precache-manifest.2117043de687fb547c65955b7257e68c.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af2098197e023876950",
+    "url": "/css/app.05f4ba6b.css"
+  },
+  {
+    "revision": "cafbe915d42a7b2be987",
+    "url": "/css/chunk-vendors.ba103142.css"
+  },
+  {
+    "revision": "bcb572d064d31c016827",
+    "url": "/css/game.2a89ca31.css"
+  },
+  {
+    "revision": "d2e4a82e4b745a9d1af4aa60427c75af",
+    "url": "/images/card.png"
+  },
+  {
+    "revision": "491f3fc9a14b9dd7190d8aa28e9e829e",
+    "url": "/images/cards.jpg"
+  },
+  {
+    "revision": "1963502749c024e1590789bab3632bc9",
+    "url": "/images/cards/10C.png"
+  },
+  {
+    "revision": "39ef422e15761450c33f8cb3ee369ebe",
+    "url": "/images/cards/10D.png"
+  },
+  {
+    "revision": "60ab387c8c1c50dd3a75255500218b94",
+    "url": "/images/cards/10H.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/images/cards/10S.png"
+  },
+  {
+    "revision": "136dab439961d61f4b9c1f939e07828d",
+    "url": "/images/cards/2C.png"
+  },
+  {
+    "revision": "1451de5d494406e395215276a2489a7c",
+    "url": "/images/cards/2D.png"
+  },
+  {
+    "revision": "ebe434953cb5c82ec5c8e38b8e735489",
+    "url": "/images/cards/2H.png"
+  },
+  {
+    "revision": "33c964ae685ff65233d2ac2c35b96072",
+    "url": "/images/cards/2S.png"
+  },
+  {
+    "revision": "6ed5bf64f008d72e83c67f81468ab7c5",
+    "url": "/images/cards/3C.png"
+  },
+  {
+    "revision": "690d5a477dfe9ed93261bd814c0a40d6",
+    "url": "/images/cards/3D.png"
+  },
+  {
+    "revision": "7278d51dda50bd4b8c8cadb7fe349953",
+    "url": "/images/cards/3H.png"
+  },
+  {
+    "revision": "9638bf9f29919b027765992a81f6d39b",
+    "url": "/images/cards/3S.png"
+  },
+  {
+    "revision": "23c51e0b449e342f8b7f6c8fd57e2137",
+    "url": "/images/cards/4C.png"
+  },
+  {
+    "revision": "89eaac32133444fe40876cb51f697f13",
+    "url": "/images/cards/4D.png"
+  },
+  {
+    "revision": "6695f1d514765572dfe7e5dd0863dfce",
+    "url": "/images/cards/4H.png"
+  },
+  {
+    "revision": "5156259e1f30dece1376dc5695a9a1d4",
+    "url": "/images/cards/4S.png"
+  },
+  {
+    "revision": "3ac88eddd1ac03bfc901de76424b5aba",
+    "url": "/images/cards/5C.png"
+  },
+  {
+    "revision": "24fd440cbb52affc5242a507c9dec4d1",
+    "url": "/images/cards/5D.png"
+  },
+  {
+    "revision": "5457f189cabc0476511c7ed1421b419d",
+    "url": "/images/cards/5H.png"
+  },
+  {
+    "revision": "5ab3d8627d0c4d17c86c73d90817900b",
+    "url": "/images/cards/5S.png"
+  },
+  {
+    "revision": "5c96f40f637a6b2ed34d5e837d81ef7b",
+    "url": "/images/cards/6C.png"
+  },
+  {
+    "revision": "ff1c62992cf49b6164401d1d9f77a20c",
+    "url": "/images/cards/6D.png"
+  },
+  {
+    "revision": "84564e31bf0a0e8bcd5fdd3d4aa12cc3",
+    "url": "/images/cards/6H.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/images/cards/6S.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/images/cards/7C.png"
+  },
+  {
+    "revision": "58d872f063752027e59e15af4d090123",
+    "url": "/images/cards/7D.png"
+  },
+  {
+    "revision": "c8475b530ea9f0970ff829c97f3e7abf",
+    "url": "/images/cards/7H.png"
+  },
+  {
+    "revision": "82d62864ff67ff7b5e20150316d26872",
+    "url": "/images/cards/7S.png"
+  },
+  {
+    "revision": "a46a54d93ff6e543497d205db97fd460",
+    "url": "/images/cards/8C.png"
+  },
+  {
+    "revision": "fcafeaf2d610109aa1916208080ab711",
+    "url": "/images/cards/8D.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/images/cards/8H.png"
+  },
+  {
+    "revision": "497eed1efa7e554de5b62bc9eb2b5ae6",
+    "url": "/images/cards/8S.png"
+  },
+  {
+    "revision": "4f8d0d8f2ebaeacf99e7134221d6da49",
+    "url": "/images/cards/9C.png"
+  },
+  {
+    "revision": "1d495bb1ebf632dca66a538b61bae19a",
+    "url": "/images/cards/9D.png"
+  },
+  {
+    "revision": "33ab7891b4259ea8d8823a352ef38cdf",
+    "url": "/images/cards/9H.png"
+  },
+  {
+    "revision": "50caac183ef66afe6cdda1d2e3daafda",
+    "url": "/images/cards/9S.png"
+  },
+  {
+    "revision": "17ca9f77b395a41c6d45cbff350573bb",
+    "url": "/images/cards/AC.png"
+  },
+  {
+    "revision": "f37d608fb18056bc4f83737d633c5cf5",
+    "url": "/images/cards/AD.png"
+  },
+  {
+    "revision": "a762540d55018bcf43ffba8b6343c9c9",
+    "url": "/images/cards/AH.png"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/images/cards/AS.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/images/cards/JC.png"
+  },
+  {
+    "revision": "d5682eb454510b9cdf54beb9c5a0e5f5",
+    "url": "/images/cards/JD.png"
+  },
+  {
+    "revision": "fdf4ffc434ac567c6b77905cb1b11c8a",
+    "url": "/images/cards/JH.png"
+  },
+  {
+    "revision": "89d2c97e62fcaec36c80dbdd725f216f",
+    "url": "/images/cards/JS.png"
+  },
+  {
+    "revision": "aa73cfd0e548d87f4af4a18a8d2c3c8f",
+    "url": "/images/cards/KC.png"
+  },
+  {
+    "revision": "6b5b30c00309e3cfae486f543bc1809a",
+    "url": "/images/cards/KD.png"
+  },
+  {
+    "revision": "3b48ae8207b07a1f9477d7e6135ca211",
+    "url": "/images/cards/KH.png"
+  },
+  {
+    "revision": "e8d1a4cfe9d8dca64733c050e65e9dfd",
+    "url": "/images/cards/KS.png"
+  },
+  {
+    "revision": "d6296277472d7e9594d0ebee0cb82cdc",
+    "url": "/images/cards/QC.png"
+  },
+  {
+    "revision": "6940967da8669e3410fefd3a8a33aad9",
+    "url": "/images/cards/QD.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/images/cards/QH.png"
+  },
+  {
+    "revision": "9026849bcbad6fd92f7c3cb8f70e3cd4",
+    "url": "/images/cards/QS.png"
+  },
+  {
+    "revision": "8a890151c2b9cbd56a7242210babace6",
+    "url": "/images/cards/blue_back.png"
+  },
+  {
+    "revision": "db53954a64b0a647a569a1d3964135ce",
+    "url": "/images/cards/gray_back.png"
+  },
+  {
+    "revision": "80d296e9d8cb90b9802fad8d63b71dc5",
+    "url": "/images/cards/green_back.png"
+  },
+  {
+    "revision": "44961e95f84a3c3dc34b701ec6a72e6a",
+    "url": "/images/cards/purple_back.png"
+  },
+  {
+    "revision": "b27beff28165bc95efa3894baf92b637",
+    "url": "/images/cards/red_back.png"
+  },
+  {
+    "revision": "28340480d387e857bfd012d05652fd70",
+    "url": "/images/cards/yellow_back.png"
+  },
+  {
+    "revision": "fe809fe5e8eed33d4590992df9affca3",
+    "url": "/images/chip.jpg"
+  },
+  {
+    "revision": "84a01dc6c53f0d2a58a2f7ff9e17a294",
+    "url": "/images/favicon.png"
+  },
+  {
+    "revision": "d6317e8900e36193b228ca8d7a754c4e",
+    "url": "/images/playingcards.png"
+  },
+  {
+    "revision": "cefbe224f1e3b9b635c21c3332a66aa2",
+    "url": "/images/table2.jpg"
+  },
+  {
+    "revision": "5a465f59b959595515c0ac25981ae32f",
+    "url": "/images/wood.jpg"
+  },
+  {
+    "revision": "d23aed256be432387d7fbb272b885f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af2098197e023876950",
+    "url": "/js/app.635c4ac6.js"
+  },
+  {
+    "revision": "cafbe915d42a7b2be987",
+    "url": "/js/chunk-vendors.1e0044b7.js"
+  },
+  {
+    "revision": "bcb572d064d31c016827",
+    "url": "/js/game.e9a24b9c.js"
+  },
+  {
+    "revision": "03e051649a3be210a78f166bdbd07aee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
